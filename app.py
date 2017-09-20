be2fa739e3a01435c06ec63377a821e471816caf
anonymous0010 <ctoxtli@gmail.com>
2017-09-20 10:40:47 -0400

    New changes

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
-import feedparser
-feed = feedparser.parse('http://reddit.com/.rss')
-for entry in feed['entries']:
-    print(entry['title'])
+import json, time, feedparser, schedule
+
+def get_config():
+	with open("config.json") as json_data:
+		return json.load(json_data)
+	raise
+
+def extract():
+	config = get_config()
+	for source in config["sources"]:
+		print(source["name"])
+		feed = feedparser.parse(source["url"])
+		for entry in feed["entries"]:
+			print(entry["title"])
+
+def cron():
+	config = get_config()
+	schedule.every(config["time"]).minutes.do(extract)
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
+
+def init():
+	extract()
+	cron()
+
+init()
