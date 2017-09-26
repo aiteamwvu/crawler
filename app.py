f5d32a570a5f8af5abe068ff566c7ee3a80b21ae
anonymous0010 <ctoxtli@gmail.com>
2017-09-26 12:35:01 -0400

    New changes

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ import json, time, feedparser, schedule, config, threading, pymongo
 from neo4j.v1 import GraphDatabase, basic_auth
 
 class Crawler:
-	conn = None
+	conn_mongo = None
+	conn_neo4j = None
 
-	def get_neu4j(self):
-		if self.conn:
-			return self.conn.session()
-		self.conn = GraphDatabase.driver(config.neo4j_host, auth=basic_auth(config.neo4j_user, config.neo4j_pass))
-		return self.conn.session()
+	def get_neo4j(self):
+		if self.conn_neo4j:
+			return self.conn_neo4j.session()
+		self.conn_neo4j = GraphDatabase.driver(config.neo4j_host, auth=basic_auth(config.neo4j_user, config.neo4j_pass))
+		return self.conn_neo4j.session()
 
 	def get_mongo(self):
-		if self.conn:
-			return self.conn
-		self.conn = pymongo.MongoClient()[config.mongo_db]
-		return self.conn
+		if self.conn_mongo:
+			return self.conn_mongo
+		self.conn_mongo = pymongo.MongoClient()[config.mongo_db]
+		return self.conn_mongo
 
 	def extract(self, callback=None):
 		for source in config.sources:
@@ -23,15 +24,53 @@ class Crawler:
 				article = json.loads(json.dumps(entry, default=str))
 				article.update(source)
 				article["_id"] = article["link"]
-				print(json.dumps(article, indent=4, sort_keys=True))
+				#print(json.dumps(article, indent=4, sort_keys=True))
 				if callback:
 					callback(article, source["source_table"])
 
 	def extract_and_save(self):
-		self.extract(self.insert_mongo)
+		self.extract(self.insert_all)
+
+	def insert_all(self, value, table="Node"):
+		self.insert_mongo(value, table)
+		self.insert_neo4j(value, table)
 
 	def insert_neo4j(self, value, table="Node"):
-		self.get_neu4j().run(statement="CREATE (x:Article) SET x = {value}", parameters={'table': table, 'value': value})
+		labels = ["author_detail", "summary_detail", "title_detail", "subtitle_detail", "source"]
+		for label in labels:
+			if label in value:
+				for i in value[label]:
+					if value[label][i] is not None:
+						value[label + "_" + i] = value[label][i]
+				del value[label]
+		labels = ["tags", "links", "content", "authors", "media_content", "media_thumbnail"]
+		for label in labels:
+			if label in value:
+				for obj in value[label]:
+					for j in obj:
+						index =label + "_" + j
+						if index not in value:
+							value[index] = []
+						if obj[j] is not None:
+							value[index].append(obj[j])
+				del value[label]
+		try:
+			self.get_neo4j().run(statement=("CREATE (x:%s) SET x = {value}" % (table)), parameters={'value': value})
+			print("INSERTED")
+			labels = ["tags_term", "source_categories"]
+			for label in labels:
+				query = """
+				MATCH (f:%s {_id:"%s"})
+				UNWIND f.%s as value
+				MATCH (a) WHERE ANY(item IN a.%s WHERE item =~ value AND a._id <> "%s")
+				CREATE (f)-[:%s {%s: value}]->(a)
+				""" % (table, value["_id"], label, label, value["_id"], label.upper(), label)
+				self.get_neo4j().run(statement=query)
+				print("LINKED")
+		except Exception as e:
+			if "already exists" not in str(e):
+				print(json.dumps(value, indent=4, sort_keys=True))
+				print(e)
 
 	def insert_mongo(self, value, table="Node"):
 		self.get_mongo()[table].save(value)
--- config.py
+++ config.py
@@ -1,41 +1,48 @@
 time = 1
 neo4j_host = "bolt://localhost:7687"
 neo4j_user = "neo4j"
-neo4j_pass = "<pass>"
+neo4j_pass = "password"
 mongo_db = "test"
 sources = [{
     "source_table": "Article",
 	"source_name": "Futurism",
 	"source_url": "https://futurism.com/feed/",
-	"source_categories": ["future", "video"]
+	"source_content": "text",
+	"source_categories": ["future"]
 }, {
     "source_table": "Article",
 	"source_name": "TechCrunch Gadgets",
 	"source_url": "http://feeds.feedburner.com/crunchgear",
-	"source_categories": ["gadgets", "text"]
+	"source_content": "text",
+	"source_categories": ["gadgets"]
 }, {
     "source_table": "Article",
 	"source_name": "Wired Gadgets",
 	"source_url": "https://www.wired.com/feed/category/gear/latest/rss",
-	"source_categories": ["gadgets", "text"]
+	"source_content": "text",
+	"source_categories": ["gadgets"]
 }, {
     "source_table": "Article",
 	"source_name": "Tech Radar Software",
 	"source_url": "http://www.techradar.com/rss/news/software",
-	"source_categories": ["software", "text"]
+	"source_content": "text",
+	"source_categories": ["software"]
 }, {
     "source_table": "Article",
 	"source_name": "CNET Videos",
 	"source_url": "http://feed.cnet.com/feed/podcast/all/hd.xml",
-	"source_categories": ["general", "video"]
+	"source_content": "video",
+	"source_categories": ["general"]
 }, {
     "source_table": "Article",
 	"source_name": "IEEE Spectrum Videos",
 	"source_url": "https://spectrum.ieee.org/rss/videos",
-	"source_categories": ["general", "video"]
+	"source_content": "video",
+	"source_categories": ["general"]
 }, {
     "source_table": "Article",
 	"source_name": "Slashdot Developers",
 	"source_url": "http://rss.slashdot.org/Slashdot/slashdotDevelopers",
-	"source_categories": ["developer", "text"]
+	"source_content": "text",
+	"source_categories": ["developer"]
 }]
